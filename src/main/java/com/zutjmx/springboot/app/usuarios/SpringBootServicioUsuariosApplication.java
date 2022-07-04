478a664d48ff9e492c03832be9c1999fcd115bb3
Jesus Zuñiga <jesuszt@hotmail.com>
2022-07-04 17:42:07 -0500

    first commit

---
+++ src/main/java/com/zutjmx/springboot/app/usuarios/SpringBootServicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.zutjmx.springboot.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioUsuariosApplication.class, args);
+	}
+
+}
