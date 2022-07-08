60c9141cc02bcd93497a2f4d634f59b8b4e5d52d
Jesus Zuñiga <jesuszt@hotmail.com>
2022-07-08 15:03:15 -0500

    Modificaciones del día 08/07/2022

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into usuarios (username,password,enabled,nombre, apellido, email) values('zutjmx','$2a$10$O8D1RIyu0adZsOQWgCqO5uMJPXwqa4ZkoTngHBxi7/JxgFcW6K3sm',1,'Jesús','Zúñiga','zutjmx@gmail.com');
-insert into usuarios (username,password,enabled,nombre, apellido, email) values('admin','$2a$10$Itdcg3P5oC8qcm0cUDcIeOv1umHnAG8h1COxJ/e5Ba9/MtEgNbBwO',1,'Brian','Kernighan','brian.kernighan@plannine.org');
+insert into usuarios (username,password,enabled,nombre, apellido, email) values('zutjmx','$2a$10$O8D1RIyu0adZsOQWgCqO5uMJPXwqa4ZkoTngHBxi7/JxgFcW6K3sm',true,'Jesús','Zúñiga','zutjmx@gmail.com');
+insert into usuarios (username,password,enabled,nombre, apellido, email) values('admin','$2a$10$Itdcg3P5oC8qcm0cUDcIeOv1umHnAG8h1COxJ/e5Ba9/MtEgNbBwO',true,'Brian','Kernighan','brian.kernighan@plannine.org');
 
 insert into roles (nombre) values('ROLE_USER');
 insert into roles (nombre) values('ROLE_ADMIN');
